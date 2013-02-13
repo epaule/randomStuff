7b2b40ececb161274b77bf33767fdce35de5e7e5
Michael Paulini <michael.paulini@wormbase.org>
2013-02-13 10:41:52 -0500

    first attempt

---
+++ MrAndersonStrikesBack.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+# the Anderson File is from WS195
+# list of IDs to redo: /nfs/wormpub/DATABASES/geneace/WGS/ANDERSEN/mapping_errors.txt
+
+use strict;
+use Getopt::Long;
+
+my ($toVersion,$varIDs);
+my $idMappingFile='/nfs/wormpub/DATABASES/geneace/WGS/ANDERSEN/ids.txt';
+my $originalGFF  ='/nfs/wormpub/DATABASES/geneace/WGS/ANDERSEN/andersen_data.ws195_collapsed';
+
+GetOptions(
+    'wnvar2anderson' => \$idMappingFile,
+    'toVersion=s'    => \$toVersion,
+    'varIDs=s'       => \$varIDs,
+)||die(@!);
+
+# a get Anderson# mappings to WBVarIDs
+my %var2anderson;
+open INF, $idMappingFile;
+while (<INF>){
+    chomp;
+    my ($wbvar,$andersonID)=split;
+    $var2anderson{$wbvar}=$andersonID;
+}
+close INF;
+
+# get varIDs from file
+my @ids;
+open INF, $varIDs;
+while (<INF>){
+    if (/(WBVar\d+)/){
+        push(@ids,$var2anderson{$1});
+    }
+}
+close INF;
+
+# create a tmp GFF2 file
+foreach my $i (@ids){
+  `grep $i $originalGFF >>!/tmp/andersonsubset.195.gff`;   
+}
+
+system("$ENV{CVS_DIR}/scripts/remap_gff_between_releases.pl -species elegans -gff /tmp/andersonsubset.195.gff -release1 195 -release2 $toVersion -outfile /tmp/andersonsubset.${toVersion}.gff")&&die(@!);
+
