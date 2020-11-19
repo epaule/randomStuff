256c19b96636e697c57f6cd3c73e197f067faad3
Michael Paulini <michael.paulini@wormbase.org>
2020-11-19 11:07:42 +0000

    first draft

---
+++ tier2_ftp_hack.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/env perl
+# copy report files to thr FTP site
+
+use Getopt::Long;
+use lib $ENV{CVS_DIR};
+use Log_files;
+use Wormbase;
+
+my ($debug,$test,$testout,$WS_version);
+GetOptions(
+	'debug=s' => \$debug,
+	'testout=s'  => \$testout,
+	'test' => \$test,
+	'release=s' => \$WS_version,
+)||die(@!);
+
+
+my $wormbase = Wormbase->new(-debug => $debug,-test => $test);
+my $log = Log_files->make_build_log($wormbase);
+
+$WS_version ||= $wormbase->get_wormbase_version;
+my $WS_version_name = "WS${WS_version}";
+
+my $targetdir = ($test) 
+    ? "$testout/releases/$WS_version_name"
+    : $wormbase->ftp_site . "/releases/.${WS_version_name}";
+
+$log->write_to("WRITING TO $targetdir\n");
+
+
+foreach my $wb (values %{$wormbase->species_accessors}) {
+    next if $wb->species eq 'elegans';
+
+    my $gspecies = $wb->full_name('-g_species'=>1);
+    my $bioproj = $wb->ncbi_bioproject;
+
+    my $in_prefix = $wb->reports . "/" . $wb->species . ".";
+    my $out_prefix = "$targetdir/species/$gspecies/$bioproj/annotation/$gspecies.${bioproj}.${WS_version_name}.";
+
+    my @files = ('functional_descriptions.txt','orthologs.txt','protein_domains.csv','geneIDs.txt','geneOtherIDs.txt','alaska_ids.tsv','uniprot_papers.txt');
+
+    foreach my $file (@files) {  
+      my $in_file = $in_prefix . "/" . $wb->species .".$file";
+      my $out_file = $out_prefix . $file . '.gz';
+      
+      if (-e $in_file) {
+        $log->write_to("WARNING: $in_file is empty\n") unless -s $in_file;
+        $wormbase->run_command("cat $in_file | gzip -n -9 -c > $out_file", $log);
+      } else {
+        $log->write_to("WARNING: can't find $in_file\n");
+      }
+ 
+      $log->write_to("WARNING: $out_file is empty\n") unless -s $out_file;
+    }
+}
+$log->mail;
